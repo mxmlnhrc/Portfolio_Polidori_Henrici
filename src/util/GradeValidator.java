02e0b4aecba13018debe2fd3fa932c03c440767e
mxmlnhrc <53120138+mxmlnhrc@users.noreply.github.com>
2025-05-14 22:35:34 +0200

    Kommentar hinzugefügt um nacher einfach die Notenskalen zu ändern.

--- src/util/GradeValidator.java
+++ src/util/GradeValidator.java
@@ -6,7 +6,7 @@ import exception.ValidationException;
 public class GradeValidator implements Validator<Double> {
     @Override
     public void validate(Double input) throws ValidationException {
-        if (input == null || input < 0.0 || input > 100.0) {
+        if (input == null || input < 0.0 || input > 100.0) {  // Annahme von Noten zwischen 0 und 100 (Prozent) | Alt: 1.0 - 6.0 / Unistandard
             throw new InvalidGradeException(input);
         }
     }
