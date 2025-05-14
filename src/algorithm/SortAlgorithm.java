9a8d3a1ec486701c3a9bc1b1892779019f266515
mxmlnhrc <53120138+mxmlnhrc@users.noreply.github.com>
2025-05-14 22:09:35 +0200

    Moved ValidationException

--- src/algorithm/SortAlgorithm.java
+++ src/algorithm/SortAlgorithm.java
@@ -1,4 +1,11 @@
 package algorithm;
 
-public interface SortAlgorithm {
+import java.util.Comparator;
+import java.util.List;
+
+public interface SortAlgorithm<T> {
+    /**
+     * Sortiert die gegebene Liste anhand des Comparators.
+     */
+    void sort(List<T> list, Comparator<T> comparator);
 }
--- src/datastructure/EigeneListe.java
+++ src/datastructure/EigeneListe.java
@@ -1,4 +1,12 @@
 package datastructure;
 
-public interface EigeneListe {
+import java.util.Iterator;
+
+public interface EigeneListe<T> extends Iterable<T> {
+    void add(T element); // Fügt ein Element am Ende der Liste hinzu
+    boolean remove(T element); // Entfernt das erste Vorkommen eines Elements, gibt true bei Erfolg zurück
+    T get(int index); // Gibt das Element am angegebenen Index zurück
+    int size(); // Liefert die Anzahl der Elemente in der Liste
+    boolean contains(T element);  // Prüft, ob ein Element in der Liste enthalten ist
+    void clear(); // Entfernt alle Elemente aus der Liste
 }
